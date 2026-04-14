61961374f917c6a72cfa4c0f825ba337e82e816c
mohyeonMan <qkrwlgns0510@gmail.com>
2026-04-14 13:52:06 +0900

    init : 스프링 초기화

---
+++ src/main/java/io/jhpark/kopic/ws/KopicWsApplication.java
@@ -0,0 +1,13 @@
+package io.jhpark.kopic.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopicWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopicWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jhpark/kopic/ws/KopicWsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jhpark.kopic.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KopicWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
